229284d85064575090456c06776ae605e16037a5
samir717le <158018273+samir717le@users.noreply.github.com>
2024-03-06 17:06:53 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-    # Install GNOME Desktop Environment
-    sudo apt update
-    sudo apt install -y ubuntu-desktop
 
-    # Install VNC Server
-    sudo apt install -y tigervnc-standalone-server
+# Install GNOME Desktop Environment
+sudo apt update
+sudo apt install -y ubuntu-desktop
 
-    # Install noVNC
-    git clone https://github.com/novnc/noVNC.git
+# Install VNC Server
+sudo apt install -y tigervnc-standalone-server
 
-    # Install WGET GIT FIGLET
-    sudo apt install -y wget get figlet
-   #Figlet
-	figlet "vnc"
-    # Start GNOME with VNC and noVNC
-    vncstart
-    # Start VNC Server with GNOME Desktop
-    vncserver :1 -geometry 1920x1080 -depth 24
+# Install noVNC
+git clone https://github.com/novnc/noVNC.git
 
-    # Start noVNC Server
-    cd /path/to/noVNC
-    ./utils/launch.sh --vnc localhost:5901
+# Install WGET GIT FIGLET
+sudo apt install -y wget git figlet
 
+# Figlet
+figlet "vnc"
+
+# Start VNC Server with GNOME Desktop
+vncserver :1 -geometry 1920x1080 -depth 24
+
+# Start noVNC Server
+cd noVNC
+./utils/launch.sh --vnc localhost:5901
+
+# Move vncstart.sh to /usr/local/bin (if needed)
 mv vncstart.sh /usr/local/bin
